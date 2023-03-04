c5fb36a3ade6da135cfa61045f2c5a0192062a67
ChazzitUP <hamzarezgui.education@gmail.com>
2023-03-04 19:15:33 +0100

    navbar scroll behaviour

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
           <span className="text">fiverr</span>
@@ -15,15 +28,20 @@ const Navbar = () => {
           <span>English</span>
           <span>Sign in</span>
           <span>Become a seller</span>
-          <button>Sign in</button>
+          <button>Join</button>
         </div>
       </div>
-      <hr />
-      <div className="menu">
-        <span>test</span>
-        <span>test</span>
-        <span>test</span>
-      </div>
+
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>test</span>
+            <span>test</span>
+            <span>test</span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
