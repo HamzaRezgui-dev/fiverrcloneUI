76db1f698ef6daa8eb9b0aa5cf11464778b26a10
ChazzitUP <hamzarezgui.education@gmail.com>
2023-03-17 20:21:15 +0100

    adding menu using static user

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,6 +3,7 @@ import "./navbar.scss";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
+  const [Open, setOpen] = useState(false);
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
   };
@@ -14,6 +15,12 @@ const Navbar = () => {
       window.removeEventListener("scroll", isActive);
     };
   }, []);
+
+  const currentUser = {
+    id: 1,
+    userName: "Hamza",
+    isSeller: true,
+  };
 
   return (
     <div className={active ? "navbar active" : "navbar"}>
@@ -27,8 +34,30 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a seller</span>
-          <button>Join</button>
+          {currentUser?.isSeller && <span>Become a seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!Open)}>
+              <img
+                src="https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg"
+                alt="angular"
+              />
+              <span>{currentUser?.userName}</span>
+              {Open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add new gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
 
