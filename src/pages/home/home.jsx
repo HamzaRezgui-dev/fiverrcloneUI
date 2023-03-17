b736abfc76ce77b51a9afd5a9821a758d2ee8617
ChazzitUP <hamzarezgui.education@gmail.com>
2023-03-17 21:35:28 +0100

    creating and designing featured component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./home.scss";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div className="home">
+      <Featured />
+      <TrustedBy />
+    </div>
+  );
 };
 
 export default Home;
